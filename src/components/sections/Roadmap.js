37c02c3333bf53eecd7667ba09aa275ebe954cb7
Gizmotronn <droidology@allianceofdroids.org.au>
2022-05-17 13:53:52 +0800

    🛰🚦 Finish roadmap & showcase components #5

--- src/components/sections/Roadmap.js
+++ src/components/sections/Roadmap.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
 import styled from 'styled-components';
 import DrawSvg from './Roadmap/DrawSvg';
+import { useRef } from 'react';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 
 const Section = styled.section`
     min-height: 100vh;
@@ -50,9 +53,24 @@ const Items = styled.ul`
 
     &>*:nth-of-type(2n +1){
         justify-content: start;
+
+        div{
+            border-radius: 50px 0 50px 0;
+            text-align:right;
+        }
+        p{
+            border-radius: 40px 0 40px
+        }
     }
     &>*:nth-of-type(2n){
         justify-content: end;
+        div{
+            border-radius: 0 50px 0 50px;
+            text-align:left;
+        }
+        p{
+            border-radius: 0 40px 0 40px;
+        }
     }
 `
 
@@ -81,23 +99,21 @@ const Box = styled.p`
 const SubTitle = styled.span`
     display: block;
     font-size: ${props => props.theme.fontxl};
-    text-transform:capitalize;
     color: ${props => props.theme.text};
 `;
 
 const Text = styled.span`
     display: block;
     font-size: ${props => props.theme.fontsm};
-    text-transform:capitalize;
     color: ${props => props.theme.text};
 
     font-weight:400;
     margin: 0.5rem 0;
 `;
 
-const RoadmapItem = ({title, subtext}) => {
+const RoadmapItem = ({title, subtext, addToRef}) => {
     return(
-        <Item>
+        <Item ref={addToRef}>
             <ItemContainer>
                 <Box>
                     <SubTitle>{title} </SubTitle>
@@ -109,6 +125,43 @@ const RoadmapItem = ({title, subtext}) => {
 }
 
 const Roadmap = () => {
+    const revealRefs = useRef([]);
+    revealRefs.current = [];
+    gsap.registerPlugin(ScrollTrigger)
+
+    const addToRefs = (el) => {
+        if(el && !revealRefs.current.includes(el)) {
+            revealRefs.current.push(el);
+        }
+    }
+
+    useLayoutEffect(() => {
+        console.log(revealRefs);
+        let t1 = gsap.timeline();
+        revealRefs.current.forEach( (el, index) => {
+            t1.fromTo(
+                el.childNodes[0], {
+                    y: '0'
+                },{
+                    y: '-30%',
+
+                    scrollTrigger:{
+                        id: `section-${index + 1}`,
+                        trigger: el,
+                        start: 'top center+=200px',
+                        end: 'bottom center',
+                        scrub:true,
+                        markers:true,
+                    }
+                }
+            )
+        })
+
+        return () => {
+
+        };
+    }, [])
+
     return (
         <Section>
             <Title>Roadmap</Title>
@@ -117,12 +170,13 @@ const Roadmap = () => {
                     <DrawSvg />
                 </SvgContainer>
                 <Items>
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
-                    <RoadmapItem title="This is the title" subtext="this is subtitle" />
+                    <Item>&nbsp;</Item> {/* Empty item */}
+                        <RoadmapItem addToRef={addToRefs} title="NFT Drop" subtext="We'll be dropping a series of collectibles that will be part of the Star Sailors Cinematic Universe as eco-friendly NFTs. These tokens will provide real-world utility in the maingame, and come with a collection of in-universe minigames too!" />
+                        <RoadmapItem addToRef={addToRefs} title="ERC20 Token" subtext="Staking possibilities will open up with the debut of our token, which will allow for the creation of a community DAO for all holders. Each token will hold a piece of the initial data that will be seen in section 4" />
+                        <RoadmapItem addToRef={addToRefs} title="Community builds" subtext="This phase is all about our community. In addition to our Kickstarter campaign, we'll be creating a collection of NFTs with the help of everyone who's held our tokens in the past (or those under 18 subscribed to our mailing list). Minigames will continue to be updated, with the first chapter of the Star Sailors game being released." />
+                        <RoadmapItem addToRef={addToRefs} title="Citizen science" subtext="Your actions in-game will now bring about in-game rewards as a thankyou for contributing to real world science. Our game will draw in data from APIs like the Zooniverse Panoptes project so that by playing our game, you can help push humanity forward and gain an understanding of science along the way." />
+                        <RoadmapItem addToRef={addToRefs} title="Full DAO Integration" subtext="No more will you be required to be on your computer to contribute - our DAO will feature mobile-friendly minigames and direct access to Zooniverse surveys" />
+                        <RoadmapItem addToRef={addToRefs} title="Cross-chain support" subtext="Other chains like Solana will be directly integrated with your in-game account. And there's still even more to come in 2023." />
                 </Items>
             </Container>
         </Section>
